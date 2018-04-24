c05d9326af99801f780640ff6193efded2cdf97c
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-24 13:04:53 +0100

    Add files via upload

---
+++ hotel/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "juuheeeee950625.xyz";
+$database = "u375181454_hotel";
+$username = "u375181454_juhee";
+$password = "dst1738";
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
