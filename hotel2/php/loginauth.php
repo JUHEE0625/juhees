5a39e09408b8c514ae5d4f61808f208f5e4e0160
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 02:09:04 +0100

    Update loginauth.php

--- hotel2/php/loginauth.php
+++ hotel2/php/loginauth.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
-$link = mysqli_connect("localhost", "root", "", "hotel");
+$servername = "mysql.hostinger.kr";
+$database = "u375181454_hotel";
+$username = "u375181454_juhee";
+$password = "dst1738";
+// Create connection
+$link = mysqli_connect($servername, $username, $password, $database);
 $_SESSION['username'] = $_POST['username'];
 $_SESSION['password'] =  $_POST['password'];
 
 
 include './auth.php';
-$re = mysqli_query($link,"SELECT * from Staff where S_name = '".$_SESSION['username']."'  AND Password = '".$_SESSION['password']."' " );
+$re = mysqli_query($link,"SELECT * from Staffs where S_name = '".$_SESSION['username']."'  AND Password = '".$_SESSION['password']."' " );
 echo mysqli_error($link);
 if(mysqli_num_rows($re) > 0)
 {
