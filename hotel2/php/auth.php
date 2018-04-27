4c9f190982fb0611371fc33ec0599ca1806a3100
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 01:38:41 +0100

    Update auth.php

--- hotel2/php/auth.php
+++ hotel2/php/auth.php
@@ -10,7 +10,5 @@ $dbConnect = mysqli_connect($servername, $username, $password, $database);
 if (!$dbConnect) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$db = "u375181454_tempp";
-$selected = mysqli_select_db($dbConnect,$db)
-  or die("Could not select database");
+
 ?>
