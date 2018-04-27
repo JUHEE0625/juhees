6b7c4ddbe4fcfb9057e8e792084e23c8132f4e44
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 02:46:39 +0100

    Update auth.php

--- hotel/auth.php
+++ hotel/auth.php
@@ -6,12 +6,10 @@ $username = "u375181454_juhee";
 $password = "dst1738";
 // Create connection
 $dbConnect = mysqli_connect($servername, $username, $password, $database);
-ehco 'db connected';
 // Check connection
 if (!$dbConnect) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$db = "u375181454_hotel";
-$selected = mysqli_select_db($dbConnect,$db)
-  or die("Could not select database");
+
+
 ?>
