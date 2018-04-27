a7715c522a36d111e320f0e43b91fab9b0e1f67b
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-27 01:28:07 +0100

    Update dbconnect.php

--- hotel2/php/dbconnect.php
+++ hotel2/php/dbconnect.php
@@ -5,9 +5,10 @@ $username = "u375181454_juhee";
 $password = "dst1738";
 // Create connection
 $dbConnect = mysqli_connect($servername, $username, $password, $database);
+ echo "Connect Succeed";
 // Check connection
 if (!$dbConnect) {
-    echo "Connected successfully";
+    echo "Connect failed";
     die("Connection failed: " . mysqli_connect_error());
 }
 
